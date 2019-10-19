bcc7ce8193f01cba05dd5a7046adac48053b1010
Tobias Schmidt <tobias.schmidt@knime.com>
2019-10-19 11:49:19 +0200

    basic changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://www.knime.com/themes/custom/bootstrap_knime/logo.svg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a Create-React-App build
         </p>
         <a
           className="App-link"
